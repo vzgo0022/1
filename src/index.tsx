b8503a4c0865d55d666b460e2c5ee3f55f320aee
miliko0022 <darkcaslte@gmail.com>
2019-02-23 18:03:10 +0400

    333

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-
+import { Provider } from 'react-redux';
 import './index.css';
 
 import App from './App';
 
+import store from './Store'
 
 
 ReactDOM.render(
-     <Router>
-          <App />
-     </Router>
-     , document.getElementById('root'));
-
-
+     <Provider store={store}>
+          <Router>
+               <App />
+          </Router>
+     </Provider>
+,document.getElementById('root'));
